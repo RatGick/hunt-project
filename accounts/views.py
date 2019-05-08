074d4962e18aa71a603932e4a96498d5b1faec0e
RatGick <fachion.xxx@gmail.com>
2019-05-08 23:59:36 +0200

    New log in and sign up pages, added registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 # Create your views here.
+
+
+def signup(request):
+    if request.method == 'POST':
+        # User are sending info to us
+        if request.POST['password'] == request.POST['confirm']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'User with this name already exists.'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Passwords does not match'})
+    else:
+        # User wants to get page
+        return render(request, 'accounts/signup.html')
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    # TODO Fix logout page
+    return render(request, 'accounts/login.html')
