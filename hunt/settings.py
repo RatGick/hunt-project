1aec44b1e788a7080a12e47b854d1bfbf03e3648
RatGick <fachion.xxx@gmail.com>
2019-05-08 22:43:45 +0200

    Base html template and static

--- hunt/settings.py
+++ hunt/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'hunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['hunt/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'hunt/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 try:
